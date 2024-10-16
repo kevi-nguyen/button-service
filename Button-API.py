46c221b37f07dd403f97d9368661507256ce0fd4
Kevin Nguyen <kevin.nguyen@tum.de>
2024-10-16 19:16:36 +0200

    remove unused endpoint button status

--- Button-API.py
+++ Button-API.py
@@ -6,39 +6,12 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-# Initialize serial connection
 port = '/dev/tty.usbserial-AQ027FTG'
 baudrate = 9600
 ser = serial.Serial(port, baudrate, timeout=1)
 
-# Variables to track button status
 button_was_pressed = False
 button_is_pressed = False
-
-
-@app.get("/button_status")
-def get_button_status():
-    """
-    Endpoint to get the current status of the USB button.
-    Reflects "pressed" once upon initial press and "not pressed" otherwise.
-    """
-    global button_was_pressed, button_is_pressed
-
-    # Check for data from the serial port
-    if ser.in_waiting > 0:
-        data = ser.read(ser.in_waiting)
-        button_is_pressed = True
-        if not button_was_pressed:
-            button_was_pressed = True
-            return {"status": "pressed"}
-    else:
-        button_is_pressed = False
-        if button_was_pressed:
-            button_was_pressed = False
-            return {"status": "not pressed"}
-
-    return {"status": "not pressed"}
-
 
 @app.get("/wait_for_button_press")
 def wait_for_button_press():
